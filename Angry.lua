11d8e76faa911462371cb256789c0aa0849c2ad2
sascha3467w-lgtm <sascha3467.w@gmail.com>
2026-08-29 13:57:15 +0300

    Update Angry.lua

--- Angry.lua
+++ Angry.lua
@@ -1,8 +1,12 @@
--- Angry Hub для Blox Fruits (Delta X Executor)
-local OrionLib = loadstring(game:HttpGet(('https://githubusercontent.com')))()
+-- Загрузка стабильной библиотеки интерфейса Redz
+local RedzLib = loadstring(game:HttpGet("https://githubusercontent.com"))()
 
--- Создаем стартовое окно системы ключей
-local Window = OrionLib:MakeWindow({Name = "Angry Key System", HidePremium = false, SaveConfig = true, ConfigFolder = "AngryHubConfig"})
+-- Создаем стартовое окно для ввода ключа
+local KeyWindow = RedzLib:MakeWindow({
+    Title = "Angry Key System",
+    SubTitle = "by Angry Team",
+    LoadImage = "rbxassetid://4483345998"
+})
 
 -- Переменные для автоматических функций
 _G.AutoRaceV2 = false
@@ -12,7 +16,6 @@ _G.AutoRaceV3 = false
 function AutoRaceV2Function()
     while _G.AutoRaceV2 do
         task.wait(1)
-        -- Здесь выполняется поиск Алхимика в Зеленой Зоне и сбор 3-х цветков
         print("Angry Hub: Выполняется сбор цветков на Расу V2...")
     end
 end
@@ -21,87 +24,68 @@ end
 function AutoRaceV3Function()
     while _G.AutoRaceV3 do
         task.wait(1)
-        -- Здесь выполняется квест у Ароухи (убийство боссов, сбор сундуков и т.д.)
         print("Angry Hub: Выполняется квест на Расу V3...")
     end
 end
 
--- Вкладка авторизации в стартовом окне
-local KeyTab = OrionLib:MakeTab({
-	Name = "Ввод Ключа",
-	Icon = "rbxassetid://4483345998",
-	Premium = false
-})
+-- Вкладка авторизации
+local KeyTab = KeyWindow:CreateTab("Ввод Ключа", "rbxassetid://4483345998")
+
+local KeyInput = ""
 
 -- Поле для ввода ключа
-KeyTab:AddTextbox({
-	Name = "Ключ:",
-	Default = "",
-	TextDisappear = true,
-	Callback = function(Value)
-		_G.KeyInput = Value
-	end	
-})
+KeyTab:AddTextBox("Введите ключ:", function(Value)
+    KeyInput = Value
+end)
 
 -- Кнопка проверки ключа
-KeyTab:AddButton({
-	Name = "Проверить ключ",
-	Callback = function()
-        if _G.KeyInput == "Angry20" then
-            OrionLib:MakeNotification({
-                Name = "Успешно!",
-                Content = "Ключ верный! Загрузка Angry Hub...",
-                Image = "rbxassetid://4483345998",
-                Time = 4
+KeyTab:AddButton("Проверить ключ", function()
+    if KeyInput == "Angry20" then
+        -- Уведомление об успехе
+        pcall(function()
+            game:GetService("StarterGui"):SetCore("SendNotification", {
+                Title = "Успешно!",
+                Text = "Ключ верный! Загрузка Angry Hub...",
+                Duration = 3
             })
-            task.wait(1)
-            OrionLib:DestroyGui() -- Закрываем окно авторизации
-            
-            -- СОЗДАЕМ ГЛАВНОЕ МЕНЮ "Angry"
-            local MainMenu = OrionLib:MakeWindow({Name = "Angry", HidePremium = false, SaveConfig = true, ConfigFolder = "AngryMain"})
-            
-            -- Вкладка функций
-            local RaceTab = MainMenu:MakeTab({
-                Name = "Авто-Раса",
-                Icon = "rbxassetid://4483345998",
-                Premium = false
+        end)
+        
+        task.wait(0.5)
+        KeyWindow:Destroy() -- Закрываем окно авторизации
+        
+        -- СОЗДАЕМ ГЛАВНОЕ МЕНЮ "Angry"
+        local MainMenu = RedzLib:MakeWindow({
+            Title = "Angry",
+            SubTitle = "Blox Fruits",
+            LoadImage = "rbxassetid://4483345998"
+        })
+        
+        -- Вкладка функций
+        local RaceTab = MainMenu:CreateTab("Авто-Раса", "rbxassetid://4483345998")
+        
+        -- Переключатель Авто Расы V2
+        RaceTab:AddToggle("Авто Раса V2 (Цветки)", false, function(Value)
+            _G.AutoRaceV2 = Value
+            if Value then
+                task.spawn(AutoRaceV2Function)
+            end
+        end)
+        
+        -- Переключатель Авто Расы V3
+        RaceTab:AddToggle("Авто Раса V3 (Квесты)", false, function(Value)
+            _G.AutoRaceV3 = Value
+            if Value then
+                task.spawn(AutoRaceV3Function)
+            end
+        end)
+    else
+        -- Уведомление об ошибке
+        pcall(function()
+            game:GetService("StarterGui"):SetCore("SendNotification", {
+                Title = "Ошибка",
+                Text = "Неверный ключ! Попробуйте еще раз.",
+                Duration = 3
             })
-            
-            -- Кнопка включения Авто Расы V2
-            RaceTab:AddToggle({
-                Name = "Авто Раса V2 (Цветки)",
-                Default = false,
-                Callback = function(Value)
-                    _G.AutoRaceV2 = Value
-                    if Value then
-                        task.spawn(AutoRaceV2Function)
-                    end
-                end
-            })
-            
-            -- Кнопка включения Авто Расы V3
-            RaceTab:AddToggle({
-                Name = "Авто Раса V3 (Квесты)",
-                Default = false,
-                Callback = function(Value)
-                    _G.AutoRaceV3 = Value
-                    if Value then
-                        task.spawn(AutoRaceV3Function)
-                    end
-                end
-            })
-            
-            MainMenu:Init() -- Инициализация главного меню
-        else
-            -- Если ключ неверный
-            OrionLib:MakeNotification({
-                Name = "Ошибка",
-                Content = "Неверный ключ! Попробуйте еще раз.",
-                Image = "rbxassetid://4483345998",
-                Time = 4
-            })
-        end
-	end
-})
-
-OrionLib:Init() -- Инициализация окна авторизации
+        end)
+    end
+end)
